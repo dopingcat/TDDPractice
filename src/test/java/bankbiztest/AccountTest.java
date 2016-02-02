09029579ba2dfc8f5c49c36c1cc2cf906deacf46
Vwan <dopingcat@nate.com>
2016-02-02 17:08:48 +0900

    015 Test 코드 리팩토링

--- src/test/java/bankbiztest/AccountTest.java
+++ src/test/java/bankbiztest/AccountTest.java
@@ -6,36 +6,47 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class AccountTest {
-	// 계좌 생성 테스트
+	private Account account;
+
+	private Account openAccount(int money) {
+		return new Account(money);
+	}
+
+	@Before
+	public void setup() {
+		account = openAccount(10000);
+	}
+
 	@Test
 	public void accountTest() {
-		Account account = new Account(0);
 		assertNotNull(account);
 	}
 
-	// 잔고 조회 테스트
 	@Test
 	public void getBalanceTest() {
-		Account account = new Account(10000);
 		assertEquals(10000, account.getBalance());
-
-		account = new Account(0);
-		assertEquals(0, account.getBalance());
 	}
 
-	// 입금 테스트
 	@Test
 	public void depositTest() {
-		Account account = new Account(0);
-		account.deposit(10000);
 		assertEquals(10000, account.getBalance());
+
+		account.deposit(0);
+		assertEquals(10000, account.getBalance());
+
+		account.deposit(10000);
+		assertEquals(20000, account.getBalance());
 	}
 
-	// 출금 테스트
 	@Test
 	public void withdrawTest() {
-		Account account = new Account(10000);
+		assertEquals(10000, account.getBalance());
+		account.withdraw(0);
+		assertEquals(10000, account.getBalance());
+
 		account.withdraw(10000);
 		assertEquals(0, account.getBalance());
 	}
