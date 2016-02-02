a7168f55295f31949bf82eea8459e5ca584b7e4b
Vwan <dopingcat@nate.com>
2016-02-02 14:32:34 +0900

    005 잔고 조회 테스트 추가

--- src/test/java/bankbiztest/AccountTest.java
+++ src/test/java/bankbiztest/AccountTest.java
@@ -1,15 +1,24 @@
 package bankbiztest;
 
-import static org.junit.Assert.*;
+import bankbiz.Account;
 
 import org.junit.Test;
 
-import bankbiz.Account;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
 
 public class AccountTest {
+	// 계좌 생성 테스트
 	@Test
 	public void accountTest() {
 		Account account = new Account();
 		assertNotNull(account);
+	}
+
+	// 잔고 조회 테스트
+	@Test
+	public void getBalanceTest() {
+		Account account = new Account(10000);
+		assertThat(account.getBalance(), is(10000));
 	}
 }
