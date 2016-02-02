50536acac15c449f487fd1cd0401d7e62c925c63
Vwan <dopingcat@nate.com>
2016-02-02 11:11:14 +0900

    002 실패 테스트 작성 (Account 객체 생성 및 null 체크)

--- src/test/java/bankbiztest/AccountTest.java
+++ src/test/java/bankbiztest/AccountTest.java
@@ -1,10 +1,13 @@
 package bankbiztest;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class AccountTest {
 	@Test
-	public void junitTest() {
-
+	public void accountTest() {
+		Account account = new Account();
+		assertNotNull(account);
 	}
 }
