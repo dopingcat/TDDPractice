dcfdb56729a5a4a09a5216889d68fb7cc25b54ac
Vwan <dopingcat@nate.com>
2016-02-15 15:46:39 +0900

    004 add test case testSelectDrink()

--- src/test/java/vendingmachine/test/VendingMachineTest.java
+++ src/test/java/vendingmachine/test/VendingMachineTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import vendingmachine.VendingMachine;
+import vendingmachine.drink.Drink;
 
 public class VendingMachineTest {
 	@Test	// 잔액 확인
@@ -18,5 +19,21 @@ public class VendingMachineTest {
 
 		machine.putCoin(500);
 		assertEquals("추가 투입금액 500원", 600, machine.getChaneAmount());
+	}
+
+	@Test
+	public void testSelectDrink() throws Exception {
+		VendingMachine machine = new VendingMachine();
+
+		machine.selectDrink(new Drink("Cola", 500));
+		assertEquals("500원 음료 선택", 0, machine.getChaneAmount());
+
+		machine.putCoin(500);
+		machine.selectDrink(new Drink("Cola", 500));
+		assertEquals("500원 투입 후 500원 음료 선택", 0, machine.getChaneAmount());
+
+		machine.putCoin(500);
+		machine.selectDrink(new Drink("Cola", 300));
+		assertEquals("500원 투입 후 300원 음료 선택", 200, machine.getChaneAmount());
 	}
 }
