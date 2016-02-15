f564405228ed410898337eb760186428cb2277bd
Vwan <dopingcat@nate.com>
2016-02-15 15:48:27 +0900

    006 impl Drink class

---
+++ src/main/java/vendingmachine/drink/Drink.java
@@ -0,0 +1,15 @@
+package vendingmachine.drink;
+
+public class Drink {
+	private String name;
+	private int price;
+
+	public Drink(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+}
