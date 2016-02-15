6aaf6a82e0ae77d42e527e7ca67f056cbfce6fc6
Vwan <dopingcat@nate.com>
2016-02-15 15:33:46 +0900

    002 add test case testGetChangeAmount()

--- src/test/java/vendingmachine/test/VendingMachineTest.java
+++ src/test/java/vendingmachine/test/VendingMachineTest.java
@@ -1,10 +1,22 @@
 package vendingmachine.test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
-public class VendingMachineTest {
+import vendingmachine.VendingMachine;
 
+public class VendingMachineTest {
 	@Test	// 잔액 확인
 	public void testGetChangeAmount() throws Exception {
+		VendingMachine machine = new VendingMachine();
+
+		assertEquals("투입금액 없음", 0, machine.getChaneAmount());
+
+		machine.putCoin(100);
+		assertEquals("투입금액 100원", 100, machine.getChaneAmount());
+
+		machine.putCoin(500);
+		assertEquals("추가 투입금액 500원", 600, machine.getChaneAmount());
 	}
 }
